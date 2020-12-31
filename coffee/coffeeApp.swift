c3b7403b9e4229c489a4ff21fcf22310f54fb46d
AG <champtoday@gmail.com>
2020-12-30 19:08:29 -0500

    Initial Commit

---
+++ coffee/coffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeApp.swift
+//  coffee
+//
+//  Created by Adam Geiger on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct coffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
